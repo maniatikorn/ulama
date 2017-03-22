34a0cc9998a3a31e4e5e09fcb49ce789093ec8be
Juan <jcpablo10@gmail.com>
2017-03-22 10:00:34 -0600

    Prueba mail

--- php/contacto.php
+++ php/contacto.php
@@ -1,5 +1,4 @@
 <?php
-	
 	$nombre = $_POST['nombre'];
 	$mail = $_POST['mail'];
 	$telefono = $_POST['telefono'];
@@ -8,6 +7,22 @@
 	$asunto ="Un cliente te ha contactado desde el sitio web.";
 	$header = "FROM: " . $mail;
 	$mensaje = "Nombre:" . $nombre . "\n E-Mail: " . $mail . " \n Teléfono: " . $telefono . "\n Mensaje:\n " . $descripcion . "\n";
+
+	if (mail($para, $asunto, $mensaje, $header))
+	{
+		echo "<script language='javascript'>
+		alert('Mensaje enviado, muchas gracias.');
+		window.location.href = 'http://ulamamexico.com';
+		</script>";
+	}
+	else
+	{
+		echo "Falló el envio, intente de nuevo.";
+	}
+
+
+
+	/*
 	if (isset($_POST['submit']))
 	{
 		if (mail($para, $asunto, $mensaje, $header))
@@ -29,4 +44,5 @@
 			window.location.href = 'http://ulamamexico.com';
 			</script>";
 	}
+?>*/
 ?>
